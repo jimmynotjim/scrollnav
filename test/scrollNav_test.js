3b4a9ca88162db9a97ec0e35c95a768f1fc127fb
James Wilson <jimmynotjim@yahoo.com>
2013-10-05 21:07:34 -0400

    Wrote some better tests
    Still learning the best way to apply tests

--- test/scrollNav_test.js
+++ test/scrollNav_test.js
@@ -20,46 +20,139 @@
       throws(block, [expected], [message])
   */
 
-  module('jQuery#awesome', {
-    // This will run before each test in this module.
+ module('core', {
+  // This will run before each test in this module.
     setup: function() {
       this.elems = $('#qunit-fixture');
     }
-  });
+ });
 
-  test('is chainable', function() {
+ test('is chainable', function() {
     expect(1);
     // Not a bad test to run on collection methods.
     strictEqual(this.elems.scrollNav(), this.elems, 'should be chainable');
   });
 
-  test('first section is correct tag', function() {
-    expect(1);
-    strictEqual(this.elems.scrollNav().children().first().prop('tagName'), 'SECTION', 'should be a section tag');
-  });
-
-  test('first section has correct id', function() {
-    expect(1);
-    strictEqual(this.elems.scrollNav().children().first().attr('id'), 'scrollNav-1', 'should be scrollNav-1');
-  });
-
-  test('first heading has correct text', function() {
-    expect(1);
-    strictEqual(this.elems.scrollNav().children().first().children().first().html(), 'Test Heading', 'should be Test Heading');
+  module('sections', {
+    // This will run before each test in this module.
+    setup: function() {
+      this.elems = $('#qunit-fixture');
+      this.sections = this.elems.scrollNav().children();
+    }
   });
 
   test('correct total number of sections', function() {
-    expect(1);
-    strictEqual(this.elems.scrollNav().children().length, 2, 'should be two');
+    expect(3);
+
+    strictEqual(this.sections.length, 2, 'should be two');
+    notStrictEqual(this.sections.length, 1, 'should not be one');
+    notStrictEqual(this.sections.length, 0, 'should not be zero');
   });
 
   test('correct number of children in first section', function() {
-    expect(1);
-    strictEqual(this.elems.scrollNav().children().eq(0).children().length, 4, 'should be four');
+    expect(3);
+
+    strictEqual(this.sections.eq(0).children().length, 4, 'should be four');
+    notStrictEqual(this.sections.eq(0).children().length, 3, 'should not be three');
+    notStrictEqual(this.sections.eq(0).children().length, 5, 'should not be five');
+  });
+
+  test('correct number of children in second section', function() {
+    expect(3);
+
+    strictEqual(this.sections.eq(1).children().length, 6, 'should be six');
+    notStrictEqual(this.sections.eq(1).children().length, 5, 'should not be five');
+    notStrictEqual(this.sections.eq(1).children().length, 7, 'should not be seven');
+  });
+
+  test('first section is correct tag', function() {
+    expect(4);
+    var sectionTag = this.sections.eq(0).prop('tagName');
+
+    strictEqual(sectionTag, 'SECTION', 'should be a section tag');
+    notStrictEqual(sectionTag, 'DIV', 'should not be a div tag');
+    notStrictEqual(sectionTag, 'H2', 'should not be an h2 tag');
+    notStrictEqual(sectionTag, 'P', 'should not be a p tag');
   });
 
   test('second section is correct tag', function() {
-    expect(1);
-    strictEqual(this.elems.scrollNav().children().eq(1).prop('tagName'), 'SECTION', 'should be a section tag');
+    expect(4);
+    var sectionTag = this.sections.eq(1).prop('tagName');
+
+    strictEqual(sectionTag, 'SECTION', 'should be a section tag');
+    notStrictEqual(sectionTag, 'DIV', 'should not be a div tag');
+    notStrictEqual(sectionTag, 'H2', 'should not be an h2 tag');
+    notStrictEqual(sectionTag, 'P', 'should not be a p tag');
   });
+
+  test('first section has correct id', function() {
+    expect(4);
+    var sectionID = this.sections.eq(0).attr('id');
+
+    strictEqual(sectionID, 'scrollNav-1', 'should be scrollNav-1');
+    notStrictEqual(sectionID, 'scrollNav-0', 'should not be scrollNav-0');
+    notStrictEqual(sectionID, 'scrollNav-2', 'should not be scrollNav-2');
+    notStrictEqual(sectionID, 'scrollNav-1-1', 'should not be scrollNav-1-1');
+  });
+
+  test('second section has correct id', function() {
+    expect(4);
+    var sectionID = this.sections.eq(1).attr('id');
+
+    strictEqual(sectionID, 'scrollNav-2', 'should be scrollNav-2');
+    notStrictEqual(sectionID, 'scrollNav-1', 'should not be scrollNav-1');
+    notStrictEqual(sectionID, 'scrollNav-3', 'should not be scrollNav-3');
+    notStrictEqual(sectionID, 'scrollNav-2-1', 'should not be scrollNav-2-1');
+  });
+
+  module('nav', {
+    // This will run before each test in this module.
+    setup: function() {
+      this.elems = $('#qunit-fixture');
+      this.scrollNav = this.elems.scrollNav();
+      this.sections = this.scrollNav.children();
+      this.nav = this.scrollNav.siblings('.scroll-nav');
+    }
+  });
+
+  test('first nav lik has correct text', function() {
+    expect(3);
+    var headingText = this.sections.eq(0).children().eq(0).text();
+    var linkText = this.nav.find('li').eq(0).text();
+
+    strictEqual(linkText, headingText, 'should be Test Heading');
+    notStrictEqual(linkText, 'lame test markup', 'should not be lame test');
+    notStrictEqual(linkText, 'Another Test Heading', 'should not be Another Test Heading');
+  });
+
+  test('second nav lik has correct text', function() {
+    expect(3);
+    var headingText = this.sections.eq(1).children().eq(0).text();
+    var linkText = this.nav.find('li').eq(1).text();
+
+    strictEqual(linkText, headingText, 'should be Test Heading');
+    notStrictEqual(linkText, 'lame test markup', 'should not be lame test');
+    notStrictEqual(linkText, 'Test Heading', 'should not be Test Heading');
+  });
+
+  test('first nav lik has correct url', function() {
+    expect(3);
+    var sectionID = this.sections.eq(0).attr('id');
+    var linkURL = this.nav.find('li').eq(0).find('a').attr('href');
+
+    strictEqual(linkURL, '#' + sectionID, 'should be #scrollNav-1');
+    notStrictEqual(linkURL, 'lame test markup', 'should not be #scrollNav-0');
+    notStrictEqual(linkURL, 'Another Test Heading', 'should not be #scrollNav-2');
+  });
+
+  test('second nav lik has correct url', function() {
+    expect(3);
+    var sectionID = this.sections.eq(1).attr('id');
+    var linkURL = this.nav.find('li').eq(1).find('a').attr('href');
+
+    strictEqual(linkURL, '#' + sectionID, 'should be #scrollNav-1');
+    notStrictEqual(linkURL, 'lame test markup', 'should not be #scrollNav-1');
+    notStrictEqual(linkURL, 'Another Test Heading', 'should not be #scrollNav-3');
+  });
+
 }(jQuery));
