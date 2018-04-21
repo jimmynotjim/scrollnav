8b842e7567423108f0509aedcf491a2c537e3755
James Wilson <jimmynotjim@me.com>
2018-04-21 14:11:10 -0500

    Fixed a typo

--- src/util/updatePositionData.js
+++ src/util/updatePositionData.js
@@ -1,12 +1,12 @@
 import getYPosition from './getYPosition';
 
-export default function updatePostionData(data) {
+export default function updatePositionData(data) {
   data.forEach(section => {
     const sectionDom = document.querySelector(`#${section.id}`);
     section.offsetTop = getYPosition(sectionDom);
 
     if (section.subSections.length) {
-      section.subSections = updatePostionData(section.subSections);
+      section.subSections = updatePositionData(section.subSections);
     }
   });
 
