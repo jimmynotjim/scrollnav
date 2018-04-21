fe9a91cacb314ad777d85d2eebb0042958e7bfc7
James Wilson <jimmynotjim@me.com>
2018-04-21 17:40:29 -0500

    Clean up createNav test

--- test/tests/util/createNav.test.js
+++ test/tests/util/createNav.test.js
@@ -1,23 +1,19 @@
 import jestDomCustomMatchers from '@jarmee/jest-dom-custom-matchers';
 import createNav from '../../../src/util/createNav';
-import { html as content } from '../../fixtures/sectionMarkup';
 import { onlyH2Data, allData } from '../../fixtures/sectionData';
 
 expect.extend(jestDomCustomMatchers);
 
 describe('createNav', () => {
-  const options = {
-    insertTarget: null
-  };
   it('should create a nav element with the correct class name', () => {
-    const nav = createNav(onlyH2Data, options);
+    const nav = createNav(onlyH2Data);
 
     expect(nav).toBeHTMLElement('nav');
     expect(nav).toHaveClass('scroll-nav');
   });
 
   it('should contain the list of links', () => {
-    const nav = createNav(onlyH2Data, options);
+    const nav = createNav(onlyH2Data);
     const list = nav.children;
 
     expect(list.length).toBe(1);
