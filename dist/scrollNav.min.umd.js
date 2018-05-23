ab4956f0f15b0b1642356cef896500e53c18be20
James Wilson <jimmynotjim@me.com>
2018-05-22 22:14:06 -0500

    Fix global name space

--- dist/scrollNav.min.umd.js
+++ dist/scrollNav.min.umd.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
-	(global.scrollnav = factory());
+	(global.scrollNav = factory());
 }(this, (function () {
 
 function extend(defaults, options) {
